f6a8e4f744ff7be834b3c2d687179f5080092a88
JT Turner <j.tturner14@yahoo.com>
2019-11-06 19:53:42 -0500

    created questions

---
+++ assets/questions.js
@@ -0,0 +1,32 @@
+var questions = [
+    {
+        title: "Which is the correct tag to use when entering javascript?",
+        choices: ["<js>", "<javascript>", "<script>", "<scripts>"],
+        answer: "<script>"
+    },
+
+    {
+        title: "What is the term to describe the area of the web page that is viewable at any given time?",
+        choices: ["viewport", "screen", "image area", "page area"],
+        answer: "viewport",
+    },
+
+    {
+        title: "What does DOM stand for?",
+        choices: ["Document Orienting Module", "Document Object Model", "Document Orienting Model", "Document Object Module"],
+        answer: "Document Object Model",
+    },
+
+    {
+        title: "Which of the following is NOT a logical operator in JavaScript?",
+        choices: ["!", "||", "*", "&&"],
+        answer: "*",
+    },
+
+    {
+        title: "What is the correct syntax for selecting a div with the id of 'bags'?",
+        choices: ['document.getElementById("bags")','document.getElementByClassName("bags")', 'document.getElement("bags")', 'document.querySelectorAll("bags")'],
+        answer: 'document.getElementById("bags")',
+    }
+
+]
