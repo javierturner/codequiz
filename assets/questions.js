c2f8846482a67010d09b7a922eef158d7bb998af
JT Turner <j.tturner14@yahoo.com>
2019-11-09 14:02:09 -0500

    finished question generator

--- assets/questions.js
+++ assets/questions.js
@@ -30,3 +30,49 @@ var questions = [
     }
 
 ]
+
+var startQuiz = document.getElementById("startQuiz");
+var begin = document.getElementById("begin");
+
+//questions
+var questionsBox = document.getElementById("questionsBox");
+var titles = document.getElementById("titles");
+var choiceA = document.getElementById("buttonA");
+var choiceB = document.getElementById("buttonB");
+var choiceC = document.getElementById("buttonC");
+var choiceD = document.getElementById("buttonD");
+
+//make questions invisible when page first loads
+questionsBox.style.visibility = "hidden";
+
+
+var timer = 76;
+
+//click start button, then questions will appear and timer will start
+startQuiz.addEventListener("click", function () {
+    begin.style.display = "none";
+    timeLeft();
+    promptQuestions();
+});
+
+
+//function to generate questions
+function promptQuestions() {
+    questionsBox.style.visibility = "visible";
+    titles.textContent = questions[0].title;
+    choiceA.textContent = questions[0].choices[0];
+    choiceB.textContent = questions[0].choices[1];
+    choiceC.textContent = questions[0].choices[2];
+    choiceD.textContent = questions[0].choices[3];
+}
+
+//function for timer
+function timeLeft() {
+    var time = document.getElementById("timer");
+     setInterval(function () {
+        timer--;
+        time.textContent = ("Time: " + timer);
+    }, 1000)
+}
+
+
